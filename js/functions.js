072603f7b4c979b39de3569ddfaf0c49c0a91e27
gaabrielaagm <monserratgm>
2018-03-04 19:10:42 -0600

    Se agrego nuevas funciones
    Funciones de repartir de la Banca (máquina) y del jugador (usuario)

--- js/functions.js
+++ js/functions.js
@@ -2,45 +2,52 @@
 var baraja;
 var barajacopy;
 var score_usr;
-var score_maq;
+var score_banca;
 //var cartas = new Array(21);  
 var cartas_usr = [];
-var cartas_maq = [];
+var cartas_banca = [];
 var usr_termino;
-var maq_termino;
+var banca_termino;
+
+var cartaOculta_nom;
+var cartaOculta_tipo;
+var cartaOculta_valor;
 
 function inicio(){
     baraja = [
-        ["A","p",[1,10]], ["2","p",2], ["3","p",3], ["4","p",4], ["5","p",5], 
+        ["A","p",[1,11]], ["2","p",2], ["3","p",3], ["4","p",4], ["5","p",5], 
         ["6","p",6], ["7","p",7],["8","p",8], ["9","p",9], ["10","p",10],
         ["J","p",10], ["Q","p",10], ["K","p",10],
 
-        ["A","c",[1,10]], ["2","c",2], ["3","c",3], ["4","c",4], ["5","c",5], 
+        ["A","c",[1,11]], ["2","c",2], ["3","c",3], ["4","c",4], ["5","c",5], 
         ["6","c",6], ["7","c",7], ["8","c",8], ["9","c",9], ["10","c",10],
         ["J","c",10], ["Q","c",10], ["K","c",10],
 
-        ["A","t",[1,10]], ["2","t",2], ["3","t",3], ["4","t",4], ["5","t",5], 
+        ["A","t",[1,11]], ["2","t",2], ["3","t",3], ["4","t",4], ["5","t",5], 
         ["6","t",6], ["7","t",7], ["8","t",8], ["9","t",9], ["10","t",10],
         ["J","t",10], ["Q","t",10], ["K","t",10],
 
-        ["A","d",[1,10]], ["2","d",2], ["3","d",3], ["4","d",4], ["5","d",5], 
+        ["A","d",[1,11]], ["2","d",2], ["3","d",3], ["4","d",4], ["5","d",5], 
         ["6","d",6], ["7","d",7], ["8","d",8], ["9","d",9], ["10","d",10],
         ["J","d",10], ["Q","d",10], ["K","d",10]
     ];
     
     barajacopy = baraja;
     score_usr = 0;
-    score_maq = 0;
+    score_banca = 0;
 
     usr_termino = false;
-    maq_termino = false;
+    banca_termino = false;
     /*
     imprimir();
     */
 
     //PRIMERAS DOS CARTAS PARA USUARIO Y MAQUINA
-    generarCartas();
-    generarCartas();
+    generarCartasUsuario();
+    generarCartasUsuario();
+
+    generarCartasBanca("normal");
+    generarCartasBanca("oculta");
 
     
     document.getElementById("repartir").disabled = "true";
@@ -79,13 +86,7 @@ function nuevaCartaUsuario(){
         //OBTIENE EL RANDOM
         var random = Math.floor(Math.random() * (baraja.length));
         //alert(random);
-    /*
-        var tamarray = cartas.length;
-        cartas.
-        cartas[0] = baraja[random];
-        alert(cartas[tamarray]+" "+tamarray );
-        imprimirCartas();
-    */
+
         //AÑADE A LAS CARTAS QUE EL USUARIO LLEVA
         cartas_usr.push(baraja[random]);
 
@@ -96,20 +97,25 @@ function nuevaCartaUsuario(){
         
         //SI LE TOCO EL AS
         if(current_name == "A"){
-            if((score_usr+10) <= 21){ //Si As vale 10
-                current_value = 10;   //El as vale 10
+            if((score_usr+11) <= 21){ //Si As vale 10
+                current_value = 11;   //El as vale 10
             }else{                    //El as vale 1
                 current_value = 1; 
             }
         }
 
         var choise = "USUARIO: Name: " + current_name + " Type: " + current_type + " Value: " + current_value;
+        var nuevaCarta = document.createElement("img");
+        var src = "Baraja/"+current_name+current_type+".png";
+        nuevaCarta.src=src;
+        document.getElementById("cartas_usr").appendChild(nuevaCarta);
+
         //document.getElementById("cartas_usuario").innerHTML = choise;
         //alert(choise);
 
         //SUMA LOS PUNTOS 
         score_usr = parseInt(score_usr, 10) + parseInt(current_value, 10);
-        document.getElementById("score_usuario").innerHTML = "Score: " + score_usr;
+        document.getElementById("score_usuario").innerHTML = "Puntos: " + score_usr;
 
         //ELIMINAR DE LA BARAJA
         //alert(barajacopy.length);
@@ -117,19 +123,19 @@ function nuevaCartaUsuario(){
         //alert("Eliminado: " + elementoEliminado);
         //alert(barajacopy.length);
 
-        //nuevaCartaMaquina();
+        //nuevaCartaBanca();
     }
 }
 
-//Nueva carta Maquina
-function nuevaCartaMaquina(){
-    if(maq_termino == false){ //SI LA MAQUINA AUN NO TERMINA DE TOMAR CARTAS
+//Nueva carta Banca
+function generarCartasBanca(tipoCarta){
+    if(banca_termino == false){ //SI LA BANCA AUN NO TERMINA DE TOMAR CARTAS
         //OBTIENE EL RANDOM
         var random = Math.floor(Math.random() * (baraja.length));
         //alert(random);
 
         //AÑADE A LAS CARTAS QUE EL USUARIO LLEVA
-        cartas_maq.push(baraja[random]);
+        cartas_banca.push(baraja[random]);
 
         //MUESTRA LA CARTA NUEVA RANDOM
         var current_name = baraja[random][0];
@@ -138,20 +144,35 @@ function nuevaCartaMaquina(){
 
         //SI LE TOCO EL AS
         if(current_name == "A"){
-            if((score_maq+10) <= 21){ //Si As vale 10
-                current_value = 10;   //El as vale 10
+            if((score_banca+11) <= 21){ //Si As vale 11
+                current_value = 11;   //El as vale 11
             }else{                    //El as vale 1
                 current_value = 1; 
             }
         }
 
-        var choise = "MAQUINA: Name: " + current_name + " Type: " + current_type + " Value: " + current_value;
+        var choise = "BANCA: Name: " + current_name + " Type: " + current_type + " Value: " + current_value;
+        var nuevaCarta = document.createElement("img");
+        var src = "";
+        if(tipoCarta == "oculta"){
+            src = "Baraja/cartaOculta.png";
+            id = "cartaOculta";
+            cartaOculta_nom = current_name;
+            cartaOculta_tipo = current_type;
+            cartaOculta_valor = current_value;
+        }else{
+            src = "Baraja/"+current_name+current_type+".png";
+            id = "";
+        }       
+        nuevaCarta.src = src;
+        nuevaCarta.id = id;
+        document.getElementById("cartas_banca").appendChild(nuevaCarta);
         //document.getElementById("cartas_maquina").innerHTML = choise;
         //alert(choise);
 
         //SUMA LOS PUNTOS 
-        score_maq = parseInt(score_maq, 10) + parseInt(current_value, 10);
-        document.getElementById("score_maquina").innerHTML = "Score: " + score_maq;
+        score_banca = parseInt(score_banca, 10) + parseInt(current_value, 10);
+        document.getElementById("score_banca").innerHTML = "Puntos: " + score_banca;
 
         //ELIMINAR DE LA BARAJA
         //alert(baraja.length);
@@ -161,71 +182,78 @@ function nuevaCartaMaquina(){
     }
 }
 
-function generarCartas(){
+function generarCartasUsuario(){
     nuevaCartaUsuario();
     revisarPuntaje();
-    nuevaCartaMaquina();
-    revisarPuntaje();        
+    //nuevaCartaBanca();
+    //revisarPuntaje();        
 }
 
 function usuarioTermina(){
-    document.getElementById("nueva_carta").disabled = "true";
-    document.getElementById("listo").disabled = "true";
-    document.getElementById("repartir").disabled = "true";
-
+    var cartaOculta = document.getElementById("cartaOculta");
+    cartaOculta.src = "Baraja/Ap.png";
     usr_termino = true;
     revisarPuntaje();
-    for(i=0;i<21;i++){
-        if(maq_termino == false){
-            nuevaCartaMaquina();
-            revisarPuntaje();
-        }
+    if(score_banca < 17){
+        generarCartasBanca();
     }
+    banca_termino = true;
+    revisarPuntaje();
 }
 
 function reiniciar(){
     var baraja;
     var barajacopy;
     var score_usr = 0;
-    var score_maq = 0;
+    var score_banca = 0;
     var cartas_usr = [];
-    var cartas_maq = [];
+    var cartas_banca = [];
     inicio();
+}
+
+function mostrarCartaOculta(){
+    var cartaOculta = document.getElementById("cartaOculta");
+    cartaOculta.src = "Baraja/Ap.png";
 }
 
 function revisarPuntaje(){
     
-    if(score_usr == 21 && score_maq != 21){
-        document.write("<h2>USR VS MAQUI</h2><br><h1>",score_usr," vs ",score_maq,
-                        "</h1><h2>HAS GANADO, VUELVE A CARGAR LA PAGINA<h2>");
+    if(score_usr == 21 && score_banca != 21){
+        //confirm("USR VS BANCA "+score_usr+" vs "+score_banca+" HAS GANADO, VUELVE A CARGAR LA PAGINA ");
+        mostrarCartaOculta();
+        window.open("https://www.argar.cat", "Diseño Web", "width=300, height=200");
     }else{
         if(score_usr > 21){
-            document.write("<h2>USR VS MAQUI</h2><br><h1>",score_usr," vs ",score_maq,
-                            "<h2>HAS PERDIDO, VUELVE A CARGAR LA PAGINA<h2>");
+            mostrarCartaOculta();
+            //confirm("USR VS BANCA "+score_usr+" vs "+score_banca+" HAS PERDIDO, VUELVE A CARGAR LA PAGINA");
+            window.open("https://www.argar.cat", "Diseño Web", "width=300, height=200");
         }
     }
     
-    if(score_maq == 21){
-        document.write("<h2>USR VS MAQUI</h2><br><h1>",score_usr," vs ",score_maq,
-                        "<h2>HAS PERDIDO, (LA MAQUINA TE GANO) VUELVE A CARGAR LA PAGINA<h2>");
+    if(score_banca == 21 && score_usr != 21){
+        mostrarCartaOculta();
+        //confirm("USR VS BANCA "+score_usr+" vs "+score_banca+" HAS PERDIDO, (LA MAQUINA TE GANO) VUELVE A CARGAR LA PAGINA");
+        window.open("https://www.argar.cat", "Diseño Web", "width=300, height=200");
     }else{
-        if(score_maq > 21 && score_usr != 21){
-            document.write("<h2>USR VS MAQUI</h2><br><h1>",score_usr," vs ",score_maq,
-                            "<h2>HAS GANADO, (LA MAQUINA PERDIO) VUELVE A CARGAR LA PAGINA<h2>");
+        if(score_banca > 21 && score_usr != 21){
+            mostrarCartaOculta();
+            //confirm("USR VS BANCA "+score_usr+" vs "+score_banca+" HAS GANADO, (LA MAQUINA PERDIO) VUELVE A CARGAR LA PAGINA");
+            window.open("https://www.argar.cat", "Diseño Web", "width=300, height=200");
         }else{
-            if(score_maq >= 18 && score_maq < 21){
-                maq_termino = true;
+            if(score_banca >= 18 && score_banca < 21){
+                banca_termino = true;
                 if(usr_termino == true){
-                    document.write("<h2>USR VS MAQUI</h2><br><h1>",score_usr," vs ",score_maq,
-                        "<h2>HAS PERDIDO, (LA MAQUINA TE GANO) VUELVE A CARGAR LA PAGINA<h2>");
+                    mostrarCartaOculta();
+                    //confirm("USR VS BANCA "+score_usr+" vs "+score_banca+" HAS PERDIDO, (LA MAQUINA TE GANO) VUELVE A CARGAR LA PAGINA");
+                    window.open("https://www.argar.cat", "Diseño Web", "width=300, height=200");
                 }
             }
         }
     }
 
     //EMPATE
-    if(score_maq == score_usr && usr_termino == true && maq_termino == true){
-        document.write("<h2>USR VS MAQUI</h2><br><h1>",score_usr," vs ",score_maq,
-                            "<h2>EMPATE!<h2>");
+    if(score_banca == score_usr && usr_termino == true && banca_termino == true){
+        //confirm("USR VS BANCA "+score_usr+" vs "+score_banca+" EMPATE! ");
+        window.open("https://www.argar.cat", "Diseño Web", "width=300, height=200")
     }
 }
